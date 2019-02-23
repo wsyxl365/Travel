07b6386e8b8653cdd236e12e147b6616c2f8e193
wsyxl365 <771388996@qq.com>
2019-02-23 20:34:39 +0800

    轮播图change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img
+                    class="swiper-img"
+                    :src="item.imgUrl" alt=""
+                />
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeSwiper",
+        data() {
+            return {
+                swiperOption: {
+                    pagination: ".swiper-pagination",
+                    loop: true
+                },
+                swiperList: [{
+                    id: '0001',
+                    imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20191/a211765c0bb579b18c6e7d6d75997a48.jpg_750x200_b6921fa6.jpg"
+                }, {
+                    id: "0002",
+                    imgUrl: "http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20192/9f73976e40c4ef845cabe0efc0269ebb.jpg_750x200_aab92b7a.jpg"
+                }]
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff !important
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 26.66%
+        background: #eee
+        .swiper-img
+            width: 100%
+</style>
