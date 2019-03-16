1722c5114e35503db813ce9f0457d0ebb06656bd
wsyxl365 <771388996@qq.com>
2019-03-16 17:22:38 +0800

    add recommend

---
+++ src/pages/list/Ani.vue
@@ -0,0 +1,19 @@
+<template>
+    <transition>
+        <div name="fade">你好</div>
+    </transition>
+</template>
+
+<script>
+    export default {
+        name: "Ani"
+    }
+</script>
+
+<style scoped>
+    .fade-enter {
+        width: 100px;
+        height: 100px;
+        background: red;
+    }
+</style>
