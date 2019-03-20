c5f0edfce2e013cd68b6d1b1db50fe829538884e
wsyxl365 <771388996@qq.com>
2019-03-20 21:26:29 +0800

    city-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
-import List from '@/pages/list/List';
-import Ani from '@/pages/list/Ani';
+import City from "@/pages/city/City"
 
 Vue.use(Router)
 
@@ -14,14 +13,9 @@ export default new Router({
             component: Home
         },
         {
-            path: '/list',
-            name: 'List',
-            component: List
-        },
-        {
-            path: '/ani',
-            name: 'Ani',
-            component: Ani
+            path: '/city',
+            name: 'City',
+            component: City
         }
     ]
 })
